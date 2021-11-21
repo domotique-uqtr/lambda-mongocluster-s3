4abf5633560513b7eda218abb675e0cbcdefcf17
Guillaume Maka <guillaume.maka@gmail.com>
2021-11-21 03:26:01 -0500

    add aws-cdk code

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,28 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.95.2',
+  cdkVersion: '1.133.0',
   defaultReleaseBranch: 'main',
   name: 'mongodb-backup-lambda',
 
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
+  cdkDependencies: [
+    '@aws-cdk/aws-lambda-event-sources',
+    '@aws-cdk/aws-lambda-nodejs',
+    '@aws-cdk/aws-s3',
+    '@aws-cdk/aws-ecs',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-servicediscovery',
+    '@aws-cdk/aws-sqs',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-events',
+    '@aws-cdk/aws-events-targets',
+  ] /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */,
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
+  devDeps: [
+    'esbuild',
+    'archiver@5.0.0',
+    'dayjs@1.8.34',
+  ] /* Build dependencies for this module. */,
   // packageName: undefined,            /* The "name" in package.json. */
   // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
   // release: undefined,                /* Add release management to this project. */
